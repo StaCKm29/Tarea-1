54840605a16d6bf26b74d9917b144afe0dc5a45a
StaCKm29 <mamartinez2023@udec.cl>
2024-04-20 11:16:06 -0400

    Add Bebida and Comprador class

--- src/main/java/org/tarea1/Bebida.java
+++ src/main/java/org/tarea1/Bebida.java
@@ -1,4 +1,18 @@
 package org.tarea1;
 
-public class Bebida {
+abstract class Bebida {
+    private int serie;
+
+    /**
+     * @param serie numero de serie de una bebida
+     */
+    public Bebida(int serie){
+        this.serie = serie;
+    }
+
+    public int getSerie(){
+        return serie;
+    }
+
+    public abstract String beber();
 }
--- src/main/java/org/tarea1/Comprador.java
+++ src/main/java/org/tarea1/Comprador.java
@@ -1,4 +1,39 @@
 package org.tarea1;
 
 public class Comprador {
+    private String sonido;
+    private int vuelto = 0;
+
+    /**
+     * Constructor
+     * @param m
+     * @param cualBebida 1 para cocacola, 2 para sprite
+     * @param exp
+     */
+    public Comprador(Moneda m, int cualBebida, Expendedor exp){
+        Bebida b1 = exp.comprarBebida(m, cualBebida);
+
+        while (true) {
+            Moneda a = exp.getVuelto();
+            if (a == null) {
+                break;
+            } else {
+                vuelto += a.getValor();
+            }
+        }
+
+        if (b1 != null) {
+            sonido = b1.beber();
+        }else{
+            sonido = null;
+        }
+    }
+
+    public int cuantoVuelto(){
+        return vuelto;
+    }
+    public String queBebiste(){
+        return sonido;
+    }
+
 }
