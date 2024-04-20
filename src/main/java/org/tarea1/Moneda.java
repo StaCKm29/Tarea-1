e9bab7c7688896291b8a2e5135c7b6a32821b336
zMRGABOz <gacastillo2023@udec.cl>
2024-04-20 13:07:54 -0400

    Add more Monedas

--- src/main/java/org/tarea1/Moneda.java
+++ src/main/java/org/tarea1/Moneda.java
@@ -1,20 +1,14 @@
 package org.tarea1;
 
-<<<<<<< HEAD
+
 abstract class Moneda{
     public Moneda(){
     }
     public Moneda getSerie(){
         return this;
     }
-
     public abstract int getValor();
 
 }
-=======
-public class Moneda {
-    public Moneda(){
 
-    }
-}
->>>>>>> main
+
