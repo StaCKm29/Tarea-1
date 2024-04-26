fa6be1bb68ea681b3e7b0eb15e6e07676e5550a7
StaCKm29 <mamartinez2023@udec.cl>
2024-04-25 23:00:27 -0400

    Implementando libreria JOptionPane

--- src/main/java/org/tarea1/Main.java
+++ src/main/java/org/tarea1/Main.java
@@ -1,5 +1,7 @@
 package org.tarea1;
 
+import javax.swing.*;
+
 /**
  * Un proyecto que simula una venta a través de un expendedor de dulces y bebidas, mediante un comprador el cual maneja monedas para comprar
  * diversos productos.
@@ -10,67 +12,32 @@ package org.tarea1;
  */
 public class Main {
     public static void main(String[] args) {
-        Expendedor exp = new Expendedor(7);
+        int cantidad = Integer.parseInt(JOptionPane.showInputDialog("Ingrese la cantidad de productos en stock para el expendedor"));
+        Expendedor exp = new Expendedor(cantidad);
+        JOptionPane.showMessageDialog(null, "Bienvenido al Expendedor de dulces y bebidas.\n"
+        + "A continuación se mostrarán los productos: \n"
+        + "1.Cocacola $1000\n"+"2.Sprite $1000\n"+"3.Fanta $1000\n"+"4.Snickers $800\n"+"5.Super8 $300\n");
         Moneda m1 = null;
-        Moneda m2 = null;
-        Moneda m3 = null;
-        Moneda m4 = null;
         Moneda m5 = null;
         /**
          * Se intenta comprar un producto con una moneda especificando qué producto y el expendedor
          * En caso que la moneda no se haya instanciado correctamente, entra en el primer catch.
          * En caso que no haya stock o que el producto no exista, se entra en el segundo catch.
          * En caso que la moneda sea insuficiente para pagar el producto deseado, se entra en el tercer catch.
-         * La metodología es análoga para los demás try.
          */
-        //Caso donde se instancia correctamente. Retorna un vuelto
-        try {
-            m1 = new Moneda1000();
-            Comprador c4 = new Comprador(m1, 5, exp);
-            System.out.println(c4.queConsumiste() + ", " + c4.cuantoVuelto());
-        } catch (PagoIncorrectoException e) {
-            System.out.println(e.getMessage());
-        } catch (NoHayProductoException e) {
-            Moneda moneda = exp.getVuelto();
-            System.out.println(e.getMessage() + "Vuelto: " + moneda.getValor() + "\n");
-        } catch (PagoInsuficienteException e) {
-            Moneda moneda = exp.getVuelto();
-            System.out.println(e.getMessage() + "Vuelto: " + moneda.getValor() + "\n");
-        }
 
-        //Caso donde se intenta comprar con una moneda nula
         try {
-            Comprador c = new Comprador(m2, 4, exp);
-            System.out.println(c.queConsumiste() + ", " + c.cuantoVuelto());
-        } catch (PagoIncorrectoException e) {
-            System.out.println(e.getMessage());
-        } catch (NoHayProductoException e) {
-            Moneda moneda = exp.getVuelto();
-            System.out.println(e.getMessage() + "Vuelto: " + moneda.getValor() + "\n");
-        } catch (PagoInsuficienteException e) {
-            Moneda moneda = exp.getVuelto();
-            System.out.println(e.getMessage() + "Vuelto: " + moneda.getValor() + "\n");
-        }
-
-        //Caso donde el pago es insuficiente
-        try {
-            m3 = new Moneda500();
-            Comprador c2 = new Comprador(m3, 1, exp);
-            System.out.println(c2.queConsumiste() + ", " + c2.cuantoVuelto());
-        } catch (PagoIncorrectoException e) {
-            System.out.println(e.getMessage());
-        } catch (NoHayProductoException e) {
-        Moneda moneda = exp.getVuelto();
-        System.out.println(e.getMessage() + "Vuelto: " + moneda.getValor() + "\n");
-        } catch (PagoInsuficienteException e) {
-            Moneda moneda = exp.getVuelto();
-            System.out.println(e.getMessage() + "Vuelto: " + moneda.getValor() + "\n");
-        }
-
-        //Caso donde se intenta comprar un producto que no existe
-        try {
-            m4 = new Moneda1000();
-            Comprador c4 = new Comprador(m4, 10, exp);
+            int eleccion = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el número del producto que desea comprar"));
+            JOptionPane.showMessageDialog(null, "Monedas disponibles: 1000, 500, 100\n");
+            int tipoMoneda = Integer.parseInt(JOptionPane.showInputDialog("Ingrese el tipo de moneda que desea utilizar\n"));
+            if(tipoMoneda == 1000){
+                m1 = new Moneda1000();
+            }else if(tipoMoneda == 500) {
+                m1 = new Moneda500();
+            } else if (tipoMoneda == 100) {
+                m1 = new Moneda100();
+            }
+            Comprador c4 = new Comprador(m1, eleccion, exp);
             System.out.println(c4.queConsumiste() + ", " + c4.cuantoVuelto());
         } catch (PagoIncorrectoException e) {
             System.out.println(e.getMessage());
@@ -84,7 +51,7 @@ public class Main {
 
         //Caso donde se compra hasta agotar stock
         try {
-            for (int i = 0; i <= 7; i++) {
+            for (int i = 0; i <= cantidad; i++) {
                 m5 = new Moneda1000();
                 Comprador c3 = new Comprador(m5, 2, exp);
                 System.out.println(c3.queConsumiste() + ", " + c3.cuantoVuelto());
